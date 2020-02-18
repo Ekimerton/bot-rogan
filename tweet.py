ab7674f32b11705803315e1423c6a71a2960121f
Ekimerton <ekim0252@gmail.com>
2020-02-18 01:12:49 -0500

    Added loop for sending tweets

--- tweet.py
+++ tweet.py
@@ -1,15 +1,22 @@
 import tweepy
 import os
 from ai.predict import generate_tweet
+import time
+
+interval = 60 * 60 * 12 # 12 hours
+#interval = 15
 
 consumer_token = os.environ['consumer_token']
 consumer_token_secret = os.environ['consumer_token_secret']
 access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
 
-auth =tweepy.OAuthHandler(consumer_token, consumer_token_secret)
+auth = tweepy.OAuthHandler(consumer_token, consumer_token_secret)
 auth.set_access_token(access_token, access_token_secret)
 tp = tweepy.API(auth)
 
-tweet = generate_tweet("old", 200, 0.6)
-tp.update_status(tweet)
+while True:
+    tweet = generate_tweet("", 140, 1)
+    tp.update_status(tweet)
+    time.sleep(interval)
+
